8718eba2cee695f402649fc1ad091f7bafc8f295
mohamad zoubi <m.zoubi2@hotmail.com>
2024-01-23 07:36:54 +0300

    added a navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+
+
+
+function Navbar() {
+
+
+    return(
+        <div className="navbar">
+            <div className="nav-container">
+                <h3>Mohammad Zoubi</h3>
+                <ul className="nav-links">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+
+export default Navbar;
