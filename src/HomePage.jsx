2cbbfe0266aa9b0980b93468d9de043169cdf3b7
mohamad zoubi <m.zoubi2@hotmail.com>
2024-01-24 00:07:11 +0300

    homepage added

---
+++ src/HomePage.jsx
@@ -0,0 +1,12 @@
+
+
+const HomePage = () => {
+
+    return(
+        <div className="homepage" id="home">
+
+        </div>
+    )
+}
+
+export default HomePage;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,37 @@
-
+import { useState } from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 
 function Navbar() {
 
+    const [showLinks, setShowLinks] = useState(false);
 
     return(
         <div className="navbar">
             <div className="nav-container">
                 <h3>Mohammad Zoubi</h3>
-                <ul className="nav-links">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Projects</li>
-                    <li>Contact</li>
-                </ul>
+                <div className="links-container">
+                    <ul className="nav-links">
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+                <div  className="toggle-links" onClick={() => setShowLinks(!showLinks)} ><FontAwesomeIcon icon={faBars } size="2xl" /></div>
             </div>
+            <div className={showLinks ? "mobile-nav show-mobile" : "mobile-nav"}>
+                    <div  className="toggle-mobile" onClick={() => setShowLinks(!showLinks)} >
+                        <FontAwesomeIcon icon={faXmark } size="2xl" />
+                    </div>
+                    <ul className="mobile-links">
+                        <li onClick={() => setShowLinks(!showLinks)}><a href="#home">Home</a></li>
+                        <li onClick={() => setShowLinks(!showLinks)}><a href="#about">About</a></li>
+                        <li onClick={() => setShowLinks(!showLinks)}><a href="#projects">Projects</a></li>
+                        <li onClick={() => setShowLinks(!showLinks)}><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
         </div>
     )
 }
